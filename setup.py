3d904ece85c1863ceff5e33e974017811ede9e7c
avillanova <alexsrvillanova@gmail.com>
2023-10-19 07:25:44 -0400

    First commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='assert-files',
     packages=find_packages(include=['assert_files']),
-    version='0.1.0',
+    version='0.1.1',
     description='My first Python library',
     long_description='my first Python library',
     long_description_content_type='text/x-rst',
